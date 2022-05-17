8bc65c9f52b75570e6655fa245dad6fcd585a4a9
Zhongshi Jiang <jiangzs@nyu.edu>
2022-05-17 10:34:13 -0400

    initial scripts

---
+++ run_utils.py
@@ -0,0 +1,142 @@
+import numpy as np
+import re
+import subprocess
+import igl
+
+basepath = '/home/zhongshi/Workspace/wmtk-freeze/buildr/'
+
+apps = dict(remesh='app/Release/remeshing_app',
+            tetra='app/tetwild/Release/tetwild',
+            harmo='app/harmonic_tet/Release/wmtk_harmonic_tet_bin',
+            sec='app/Release/sec_app')
+
+
+def scale(x):
+    scale.a = x.min(axis=0)
+    y = x-scale.a
+    scale.b = y.max()
+    y = y/scale.b
+    return y
+
+
+def use_scale(x): return (x-scale.a)/scale.b
+
+
+def parse_log(regex, loglist):
+    rec = re.compile(regex)
+    timer = []
+    for t, f in loglist:
+        with open(f, 'r') as fp:
+            matches = ([l for l in fp.readlines()
+                        if rec.match(l) is not None])
+            assert len(matches) == 1
+            time = float(rec.match(matches[0]).group(1))
+            timer.append((t, time))
+    print(timer)
+    np.savetxt("timer.csv", np.asarray(timer), delimiter=",")
+
+
+def write_unit_scale_file(ref, output):
+    v, _ = igl.read_triangle_mesh(ref)
+    scale(v)
+
+    v1, f1 = igl.read_triangle_mesh(output)
+    igl.write_triangle_mesh(output + '.unit.ply', use_scale(v1), f1)
+
+
+class common_process:
+    @classmethod
+    def outpath(c):
+        return f'{c.base}/out/'
+
+    @classmethod
+    def logpath(c):
+        return f'{c.base}/log/'
+
+    @classmethod
+    def log_info(c):
+        parse_log(c.timer_regex, [
+                  (t, f'{c.logpath()}/{t}.log') for t in c.threads])
+
+    @classmethod
+    def blender_preprocess(c, t=0):
+        write_unit_scale_file(c.input, f'{c.outpath()}/{t}.obj')
+        subprocess.run('blender -b render.blend -P blender.py',
+                       shell=True, cwd=c.base)
+
+
+class fig3_sec(common_process):
+    threads = [0, 1, 2, 4, 8, 16, 32]
+    base = 'fig3-statue-sec/'
+    input = f'{base}/Sapphos_Head.stl'
+    timer_regex = r"^.*runtime (.*)$"
+    exe = apps['sec']
+
+    @classmethod
+    def run(cls):
+        for t in cls.threads[::-1]:
+            output = f'{cls.outpath()}/{t}.obj'
+            params = [basepath + cls.exe, cls.input,
+                      output] + f'-j {t}'.split()
+            print(' '.join(params))
+            with open(f'{cls.logpath()}/{t}.log', 'w') as fp:
+                subprocess.run(params, stdout=fp)
+
+
+class fig4_rem(common_process):
+    threads = [0, 1, 2, 4, 8, 16, 32]
+    base = 'fig4-hellskull-UniRem/'
+    input = f'{base}/hellSkull_126k.stl'
+    timer_regex = r"^.*runtime (.*)$"
+    exe = apps['remesh']
+
+    @classmethod
+    def run(cls):
+        for t in cls.threads[::-1]:
+            output = f'{cls.outpath()}/{t}.obj'
+            params = [basepath + cls.exe, cls.input, output] + \
+                f'-j {t} -r 0.01 -f 0'.split()
+            print(' '.join(params))
+            with open(f'{cls.logpath()}/{t}.log', 'w') as fp:
+                subprocess.run(params, stdout=fp)
+
+
+class fig7_secenv(common_process):
+    threads = [0, 1, 2, 4, 8, 16, 32]
+    base = 'fig7-eins-secenv/'
+    input = f'{base}/einstein_big.stl'
+    timer_regex = r"^.*runtime (.*)$"
+    exe = apps['sec']
+
+    @classmethod
+    def run(cls):
+        for t in cls.threads[::-1]:
+            output = f'{cls.outpath()}/{t}.obj'
+            params = [basepath + cls.exe, cls.input, output] + \
+                f'-e 1e-3 -j {t} -t 1e-2'.split()
+            print(' '.join(params))
+            with open(f'{cls.logpath()}/{t}.log', 'w') as fp:
+                subprocess.run(params, stdout=fp)
+
+
+class fig8_rem_env(common_process):
+    threads = [0, 1, 2, 4, 8, 16, 32]
+    base = 'fig8-headported-UniRemEnv/'
+    input = f'{base}/head_-_ported_-_scaled.stl'
+    timer_regex = r"^.*runtime (.*)$"
+    exe = apps['remesh']
+
+    @classmethod
+    def run(cls):
+        for t in cls.threads[::-1]:
+            output = f'{cls.outpath()}/{t}.obj'
+            params = [basepath + cls.exe, cls.input, output] + \
+                f'-e 1e-3 -j {t} -r 0.01 -f 0'.split()
+            print(' '.join(params))
+            with open(f'{cls.logpath()}/{t}.log', 'w') as fp:
+                subprocess.run(params, stdout=fp)
+
+
+if __name__ == '__main__':
+    fig4_rem.run()
+    fig4_rem.log_info()
---
+++ tet_slice.py
@@ -0,0 +1,185 @@
+import meshio
+import numpy as np
+import igl
+import re
+import subprocess
+
+from run_utils import *
+
+def slicetmesh(tetv, tett, clipper):
+    # clipper = np.array([0.9, 0.4,0.7,-2.8]) octocat
+    mesh_faces = igl.boundary_facets(tett)
+
+    def clip_and_color():
+        bc = igl.barycenter(tetv, tett)
+        x, y, z = tetv[tett].mean(axis=1).T
+        # print((bc*clipper[:3]).sum(axis=1))
+        clip_index = (bc*clipper[:3]).sum(axis=1) + clipper[3] >= 0
+        # print(clip_index)
+        # clip_index = np.where(0.9*x + 0.4*y + 0.7*z - 2.8 >= 0)[0]
+
+        fc = igl.boundary_facets(tett[clip_index])
+
+        original_faces = set(tuple(sorted(f)) for f in mesh_faces)
+        from_facet = [i for i, f in enumerate(
+            fc) if tuple(sorted(f)) in original_faces]
+        return tetv, fc, from_facet
+
+    V, F, facet = clip_and_color()
+    marker = np.zeros(len(F))
+    marker[facet] = 1
+    return V, F, marker
+
+
+def plot_clip(V, F, marker):
+    colorA = np.array([192, 192, 192])/256
+    colorB = np.array([249, 161, 94])/256
+
+    full_color = np.tile(colorA, (F.shape[0], 1))
+    full_color[marker == 1] = colorB
+
+    plt = None
+    sh = dict(width=1000, height=1000, wireframe=True)
+    vw = mp.Viewer(sh)
+    vw.add_mesh(V, F, c=full_color, shading=sh)
+    plt = mp.Subplot(plt, vw, s=[1, 2, 1])
+    vw = mp.Viewer(sh)
+
+
+class fig5_harmo(common_process):
+    base = 'fig5-gauss-harm/'
+    threads = [0, 1, 2, 4, 8, 16, 32]
+    input = f'{base}/gaussian_points_1M.ply'
+    exe = apps['harmo']
+    timer_regex = r"^.*Time cost: (.*)$"
+
+    @classmethod
+    def run(cls):
+        for t in cls.threads[::-1]:
+            output = f'{cls.outpath()}/{t}.msh'
+            params = [basepath + cls.exe, cls.input, output] + \
+                f'--harmonize -j {t}'.split()
+            print(' '.join(params))
+            with open(f'{cls.logpath()}/{t}.log', 'w') as fp:
+                subprocess.run(params, stdout=fp)
+
+    @classmethod
+    def blender_preprocess(c, t=0):
+        outname = f'{c.outpath()}/{t}.msh'
+        m = meshio.read(outname)
+        tetv, tett = m.points, m.cells[0].data
+        tetv = scale(tetv)
+        V, F, marker = slicetmesh(tetv, tett, [-1,0,0,0.5])
+        igl.write_triangle_mesh(outname + '.slice.ply',V, F[marker==0])
+        subprocess.run('blender -b render.blend -P blender.py', shell=True, cwd=c.base)
+    
+
+
+class fig6_tw(common_process):
+    base = 'fig6-octocat-tw/'
+    threads = [0, 1, 2, 4, 8, 16, 32]
+    input = f'{base}/32770.stl'
+    exe = apps['tetra']
+    timer_regex = r"^.*total time (.*)s$"
+
+    @classmethod
+    def run(cls):
+        for t in cls.threads[::-1]:
+            output = f'{cls.outpath()}/{t}.msh'
+            params = [basepath + cls.exe, '-i',cls.input, '-o',output] + \
+                f'-j {t} --max-its 10'.split()
+            print(' '.join(params))
+            with open(f'{cls.logpath()}/{t}.log', 'w') as fp:
+                subprocess.run(params, stdout=fp)
+
+    @classmethod
+    def blender_preprocess(c, t=0):
+        v, _ = igl.read_triangle_mesh(c.input)
+        scale(v)
+
+        outname = f'{c.outpath()}/{t}.msh_final.msh'
+        m = meshio.read(outname)
+        tetv, tett = m.points, m.cells[0].data
+
+        V, F, marker = slicetmesh(tetv, tett, [0.9, 0.4,0.7,-2.8])
+        V = use_scale(V)
+        igl.write_triangle_mesh(outname + '.slice0.ply',V, F[marker==0])
+        igl.write_triangle_mesh(outname + '.slice1.ply',V, F[marker==1])
+        subprocess.run('blender -b render.blend -P blender.py', shell=True, cwd=c.base)
+
+class fig1_fat(common_process):
+    base = 'fig1-fat/'
+    threads = [0, 1, 2, 4, 8, 16, 32]
+    input = f'{base}/39507.stl'
+    
+    timer_regex = r"^.*total time (.*)s$"
+
+    @classmethod
+    def run_tw(cls):
+        t = 32
+        exe = apps['tetra']
+        output = f'{cls.outpath()}/{t}.tetra.msh'
+        params = [basepath + exe, '-i',cls.input, '-o',output] + \
+            f'-j {t} --max-its 25 --sample-envelope --filter-with-input'.split()
+        print(' '.join(params))
+        with open(f'{cls.logpath()}/{t}.tw.log', 'w') as fp:
+            subprocess.run(params, stdout=fp)
+
+    @classmethod
+    def run_remenv(cls):
+        t = 32
+        exe = apps['remesh']
+        output = f'{cls.outpath()}/{t}.remesh.obj'
+        params = [basepath + exe, cls.input, output] + \
+            f'-e 1e-3 -j {t} -r 0.01 -f 0'.split()
+        print(' '.join(params))
+        with open(f'{cls.logpath()}/{t}.log', 'w') as fp:
+            subprocess.run(params, stdout=fp)
+    
+    @classmethod
+    def run_secenv(cls):
+        t = 32
+        exe = apps['sec']
+        output = f'{cls.outpath()}/{t}.sec.obj'
+        params = [basepath + exe, cls.input, output] + \
+            f'-e 1e-3 -j {t} -t 1e-2'.split()
+        print(' '.join(params))
+        with open(f'{cls.logpath()}/{t}.sec.log', 'w') as fp:
+            subprocess.run(params, stdout=fp)
+
+    @classmethod
+    def run_harmo(cls):
+        t = 32
+        output = f'{cls.outpath()}/{t}.harmo.msh'
+        params = [basepath + apps['harmo'], cls.input, output] + \
+            f'--harmonize -j {t}'.split()
+        print(' '.join(params))
+        with open(f'{cls.logpath()}/{t}.log', 'w') as fp:
+            subprocess.run(params, stdout=fp)
+
+    @classmethod
+    def run_all(cls):
+        cls.run_tw()
+        cls.run_harmo()
+        cls.run_secenv()
+        cls.run_remenv()
+
+    @classmethod
+    def blender_preprocess(c, t=0):
+        v, _ = igl.read_triangle_mesh(c.input)
+        scale(v)
+
+        outname = f'{c.outpath()}/{t}.msh_final.msh'
+        m = meshio.read(outname)
+        tetv, tett = m.points, m.cells[0].data
+
+        V, F, marker = slicetmesh(tetv, tett, [0.27, 1, 0.35, -6.6])
+        V = use_scale(V)
+        igl.write_triangle_mesh(outname + '.slice0.ply',V, F[marker==0])
+        igl.write_triangle_mesh(outname + '.slice1.ply',V, F[marker==1])
+        subprocess.run('blender -b render.blend -P blender.py', shell=True, cwd=c.base)
+   
+if __name__ == '__main__':
+    # fig6_tw.blender_preprocess(0)
+    fig1_fat.run()
+    fig1_fat.blender_preprocess(0)
